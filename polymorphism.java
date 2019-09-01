7f980df59551db3215345bbfe2f42a41b897b76c
shylix <shylix@naver.com>
2019-09-01 15:43:33 +0900

    자바언어 샘플 추가 : polymorphism.java closed #17 by shylix

---
+++ polymorphism.java
@@ -0,0 +1,32 @@
+/*
+* https://www.w3schools.com/java/java_polymorphism.asp
+*/
+class Animal {
+  public void animalSound() {
+    System.out.println("The animal makes a sound");
+  }
+}
+
+class Pig extends Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class Dog extends Animal {
+  public void animalSound() {
+    System.out.println("The dog says: bow wow");
+  }
+}
+
+class MyMainClass {
+  public static void main(String[] args) {
+    Animal myAnimal = new Animal();
+    Animal myPig = new Pig();
+    Animal myDog = new Dog();
+        
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
+  }
+}
